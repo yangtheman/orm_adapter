53b4e735f033ac07cc26987c6bd065ebc8036c2c
Yang Chung <yang.chung@spirent.com>
2013-03-29 16:36:58 -0700

    Tests still fail, but it's due to couchrest behavior

--- lib/orm_adapter/adapters/couchrest_model.rb
+++ lib/orm_adapter/adapters/couchrest_model.rb
@@ -5,19 +5,37 @@ module CouchRest
     class Base
       extend OrmAdapter::ToAdapter
 
+      def self.delete_all
+        all.each do |doc|
+          doc.destroy
+        end
+      end
+    
       class OrmAdapter < ::OrmAdapter::Base
+        # Do not consider these to be part of the class list
+        def self.except_classes
+          @@except_classes ||= []
+        end
+
+        # Gets a list of the available models for this adapter
+        def self.model_classes
+          ObjectSpace.each_object(Class).to_a.select {|klass| klass.ancestors.include? CouchRest::Model::Base }
+        end
+
+        # get a list of column names for a given class
         def column_names
           klass.properties
         end
 
+        # Get an instance by id of the model
         def get!(id)
-          klass.get!(wrap_key(id))
+          klass.find(wrap_key(id))
         end
 
-        def get(id)
-          klass.get(wrap_key(id))
-        end
+        # Get an instance by id of the model
+        alias :get :get!
 
+        # Find the first instance matching conditions
         def find_first(options = {})
           conditions, order, limit, offset = extract_conditions!(options)
           if conditions.empty?
@@ -29,7 +47,8 @@ module CouchRest
             view.key(conditions.values.first).limit(1).first
           end
         end
-
+        
+        # Find all models matching conditions
         def find_all(options = {})
           conditions, order, limit, offset = extract_conditions!(options)
           if conditions.empty?
@@ -42,12 +61,26 @@ module CouchRest
           end
         end
 
-        def create!(attributes = {})
+        # Create a model with given attributes
+        def create!(attributes)
           klass.create!(attributes)
         end
-
+        
         def destroy(object)
           object.destroy if valid_object?(object)
+        end
+  
+        protected
+
+        # converts and documents to ids
+        def conditions_to_fields(conditions)
+          conditions.inject({}) do |fields, (key, value)|
+            if value.is_a?(CouchRest::Model::Base) && klass.fields.keys.include?("#{key}_id")
+              fields.merge("#{key}_id" => value.id)
+            else
+              fields.merge(key => value)
+            end
+          end
         end
       end
     end
--- spec/orm_adapter/adapters/couchrest_model_spec.rb
+++ spec/orm_adapter/adapters/couchrest_model_spec.rb
@@ -1,32 +1,40 @@
 require 'spec_helper'
 require 'orm_adapter/example_app_shared'
 
-if !defined?(CouchRest::Model)
+if !defined?(CouchRest::Model) || !(CouchRest.database!("http://admin:admin@127.0.0.1:5984/test") rescue nil)
   puts "** require 'couchrest_model' to run the specs in #{__FILE__}"
 else  
-  
-  CouchRest::Model::Base.use_database("http://localhost:5984/test")
+
+  CouchRest.database!("http://admin:admin@127.0.0.1:5984/test")
+  CouchRest::Model::Base.connection.update(:username => "admin", :password => "admin")
+  CouchRest::Model::Base.use_database("test")
   
   module CouchRestOrmSpec
-    class User
-      include CouchRest::Model
-      field :name
-      field :rating
+    class User < CouchRest::Model::Base
+      property :name
+      property :rating
+      
+      design do
+        view :by_name
+        view :by_rating
+      end
     end
 
-    class Note
-      include CouchRest::Model
-      field :body, :default => "made by orm"
+    class Note < CouchRest::Model::Base
+      property :body, :default => "made by orm"
       belongs_to :owner, :class_name => 'CouchRestOrmSpec::User'
+      
+      design do
+        view :by_name
+      end
     end
     
     # here be the specs!
-    describe CouchRest::Model::OrmAdapter do
+    describe CouchRest::Model::Base::OrmAdapter do
       before do
         User.delete_all
         Note.delete_all
       end
-    
       it_should_behave_like "example app with orm_adapter" do
         let(:user_class) { User }
         let(:note_class) { Note }
